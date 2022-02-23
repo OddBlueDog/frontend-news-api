ea0395308115eeccf719480b7e7e99eff55acc6c
thomas <thomas01155@gmail.com>
2022-02-23 14:12:55 +0000

    Improve testing and prevent result caching to check body response and status

--- cypress/integration/news_list.js
+++ cypress/integration/news_list.js
@@ -5,29 +5,53 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-it('Check page components have loaded', () => {
-  cy.visit('http://localhost:3000');
-  cy.get('[data-cy=search-submit]').should('be.visible');
-  cy.get('[data-cy=search-input]').should('be.visible');
-  cy.get('[data-cy=news-snippet]').should('be.visible');
+beforeEach(() => {
+  cy.intercept('http://localhost:3001/**/*', { middleware: true }, (req) => {
+    req.on('before:response', (res) => {
+      // force all API responses to not be cached
+      res.headers['cache-control'] = 'no-store';
+    });
+  });
 });
 
-it('Test Search', () => {
+it('Check page components have loaded and first API call works', () => {
   cy.intercept({
     method: 'get',
     url: '/news/*',
   }).as('apiCheck');
 
   cy.visit('http://localhost:3000');
+  cy.get('[data-cy=search-submit]').should('be.visible');
+  cy.get('[data-cy=search-input]').should('be.visible');
+  cy.get('[data-cy=news-snippet]').should('be.visible');
+
   cy.wait('@apiCheck').then((interception) => {
+    expect(interception.response.statusCode).to.equal(200);
+    expect(interception.request.method).to.equal('GET');
     assert.isNotNull(interception.response.body, '1st API call has data');
   });
-  cy.get('[data-cy=news-snippet]').should('length.be.gt', 2);
 
-  cy.get('[data-cy=search-input]').type('london');
+  cy.get('[data-cy=news-snippet]').should('length.be.gt', 2);
+});
+
+it('Test Search', () => {
+  const search = 'london';
+
+  cy.visit('http://localhost:3000');
+
+  cy.intercept({
+    method: 'get',
+    url: /\/news\/\?q=(\w+)$/,
+  }).as('apiCheck');
+
+  cy.get('[data-cy=search-input]').type(search);
   cy.get('[data-cy=search-submit]').click();
+
   cy.wait('@apiCheck').then((interception) => {
+    expect(interception.response.statusCode).to.equal(200);
+    expect(interception.request.method).to.equal('GET');
     assert.isNotNull(interception.response.body, '2nd API call has data');
   });
+
   cy.get('[data-cy=news-snippet]').should('be.visible');
 });
